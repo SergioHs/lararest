6c0c225bf5c942084a22dc26d521338091ecbabb
Sergio H <shs_28@hotmail.com>
2021-01-28 11:54:45 -0300

    create tables migrations

---
+++ database/migrations/2021_01_28_100654_create_applicants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('email');
+            $table->integer('idade');
+            $table->string('linkedin');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applicants');
+    }
+}
---
+++ database/migrations/2021_01_28_101843_create_skills_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('applicant_id')->unsigned();
+            $table->foreign('applicant_id')
+                ->references('id')
+                ->on('applicants')
+                ->onDelete('cascade');
+            $table->integer('skill');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+}
